88d37a66c7881df196b125879b32369d69455203
KevinYang515 <kevin850515123@gmail.com>
2019-08-26 17:53:28 +0800

    add

---
+++ test2.py
@@ -0,0 +1,113 @@
+import os
+import sys
+import json
+import glob
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+DATA_DIR = Path('../kaggle/input')
+ROOT_DIR = Path('kaggle/working')
+
+NUM_CATS = 46
+IMAGE_SIZE = 512
+
+os.chdir('Mask_RCNN')
+sys.path.append(str(ROOT_DIR) + '/Mask_RCNN')
+from mrcnn.config import Config
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+COCO_WEIGHTS_PATH = 'mask_rcnn_coco.h5'
+
+class FashionConfig(Config):
+    NAME = "fashion"
+    NUM_CLASSES = NUM_CATS + 1 # +1 for the background class
+    
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 4 # a memory error occurs when IMAGES_PER_GPU is too high
+    
+    BACKBONE = 'resnet50'
+    
+    IMAGE_MIN_DIM = IMAGE_SIZE
+    IMAGE_MAX_DIM = IMAGE_SIZE    
+    IMAGE_RESIZE_MODE = 'none'
+    
+    RPN_ANCHOR_SCALES = (16, 32, 64, 128, 256)
+
+    STEPS_PER_EPOCH = 1000
+    VALIDATION_STEPS = 200
+
+
+with open(str(DATA_DIR) + "/label_descriptions.json") as f:
+    label_descriptions = json.load(f)
+
+label_names = [x['name'] for x in label_descriptions['categories']]
+
+def resize_image(image_path):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_AREA)  
+    return img
+
+glob_list = glob.glob('kaggle/working/fashion*/mask_rcnn_fashion_0007.h5')
+model_path = glob_list[0] if glob_list else ''
+
+class InferenceConfig(FashionConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+inference_config = InferenceConfig()
+
+model = modellib.MaskRCNN(mode='inference', 
+                          config=inference_config,
+                          model_dir=str(ROOT_DIR))
+
+assert model_path != '', "Provide path to trained weights"
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
+
+# Since the submission system does not permit overlapped masks, we have to fix them
+def refine_masks(masks, rois):
+    areas = np.sum(masks.reshape(-1, masks.shape[-1]), axis=0)
+    mask_index = np.argsort(areas)
+    union_mask = np.zeros(masks.shape[:-1], dtype=bool)
+    for m in mask_index:
+        masks[:, :, m] = np.logical_and(masks[:, :, m], np.logical_not(union_mask))
+        union_mask = np.logical_or(masks[:, :, m], union_mask)
+    for m in range(masks.shape[-1]):
+        mask_pos = np.where(masks[:, :, m]==True)
+        if np.any(mask_pos):
+            y1, x1 = np.min(mask_pos, axis=1)
+            y2, x2 = np.max(mask_pos, axis=1)
+            rois[m, :] = [y1, x1, y2, x2]
+    return masks, rois
+
+
+image_path = str(DATA_DIR) + '/4k-camera/phone/record/testimg1.PNG'
+
+img = cv2.imread(image_path)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+result = model.detect([resize_image(image_path)])
+r = result[0]
+
+if r['masks'].size > 0:
+    masks = np.zeros((img.shape[0], img.shape[1], r['masks'].shape[-1]), dtype=np.uint8)
+    for m in range(r['masks'].shape[-1]):
+        masks[:, :, m] = cv2.resize(r['masks'][:, :, m].astype('uint8'), 
+                                    (img.shape[1], img.shape[0]), interpolation=cv2.INTER_NEAREST)
+
+    y_scale = img.shape[0]/IMAGE_SIZE
+    x_scale = img.shape[1]/IMAGE_SIZE
+    rois = (r['rois'] * [y_scale, x_scale, y_scale, x_scale]).astype(int)
+
+    masks, rois = refine_masks(masks, rois)
+else:
+    masks, rois = r['masks'], r['rois']
+
+visualize.display_instances(img, rois, masks, r['class_ids'], 
+                            ['bg']+label_names, r['scores'],
+                            title="0", figsize=(12, 12))
+visualize.save_image(img,"= =" , rois, masks, r['class_ids'], r['scores'], ['bg']+label_names)
